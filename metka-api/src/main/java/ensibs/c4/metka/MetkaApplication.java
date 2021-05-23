e3e374a2bbc1a1bbdaf32a05732adf11830e8951
Alex M <alexander.matveev@univ-ubs.fr>
2021-05-24 00:04:42 +0200

    Initialize Angular and SpringBoot projects

---
+++ metka-api/src/main/java/ensibs/c4/metka/MetkaApplication.java
@@ -0,0 +1,13 @@
+package ensibs.c4.metka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetkaApplication.class, args);
+	}
+
+}
